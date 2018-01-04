ed93d25f130e49cefcb4d7f43b9fdbee382819ba
diabloevagto <diabloevagto@gmail.com>
2018-01-04 17:53:17 +0800

    美化登入頁面

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class>
+  <div class="BOX">
     <h1>{{ msg }}</h1>
-    <input type="text"
-      placeholder="your user name"
-      v-model="name">
-    <button @click="loginCheck">Login</button>
-    <router-view/>
+    <el-form ref="form"
+      label-position="top"
+      @submit.native.prevent>
+      <el-form-item label="使用者名稱">
+        <el-input v-model="name"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary"
+          @click="loginCheck">Login</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -36,20 +42,20 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  h1,
-  h2 {
-    font-weight: normal;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
+<style scoped lang="scss">
+  .BOX {
+    width: 300px;
+    height: 300px;
+    border: 2px solid #444;
+    box-shadow: 1px 5px 5px #666;
+
+    position: absolute;
+    left: 50%;
+    top: 50%;
+
+    margin-top: -150px;
+    margin-left: -150px;
+
+    padding: 10px;
   }
 </style>
